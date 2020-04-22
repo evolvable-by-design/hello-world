7c630c0cd3f3f868af06e92532df689929f24003
Antoine Cheron <cheron.antoine@gmail.com>
2020-04-22 17:26:33 +0200

    Remove logs

--- server/src/models/Talk.js
+++ server/src/models/Talk.js
@@ -1,12 +1,12 @@
 class Talk {
-  constructor(title, speaker, startTime, category) {
-    this.title = title;
-    this.speaker = speaker;
-    this.startTime = startTime;
-    this.category = category;
+  constructor (title, speaker, startTime, category) {
+    this.title = title
+    this.speaker = speaker
+    this.startTime = startTime
+    this.category = category
   }
 
-  talkRepresentation() {
+  talkRepresentation () {
     return {
       title: this.title,
       speaker: this.speaker,
@@ -14,17 +14,12 @@ class Talk {
       category: this.category
     }
   }
+}
 
-};
-
-const TaskCategoryEnum = [ 'API Design', 'API Maintenance', 'API Management' ]
+const TaskCategoryEnum = ['API Design', 'API Maintenance', 'API Management']
 
 const validateBusinessConstraints = (title, speaker, startTime, category) => {
-  console.log('Title: ' + title)
-  console.log('Speaker: ' + speaker)
-  console.log('startTime: ' + startTime)
-  console.log('category: ' + category)
-  if (!title || (title.length < 10 || title.length > 40)) {
+  if (!title || title.length < 10 || title.length > 40) {
     return false
   } else if (speaker === undefined || speaker === null) {
     return false
@@ -33,9 +28,9 @@ const validateBusinessConstraints = (title, speaker, startTime, category) => {
   } else if (!category || !TaskCategoryEnum.includes(category)) {
     return false
   } else {
-    return true;
+    return true
   }
-};
+}
 
 module.exports = {
   Talk,
